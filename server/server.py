096ea888e36f1be5a7d70ab77c3f78f8236b8081
= <=>
2025-08-18 18:32:11 +1000

    Continuing cleanup and features, preparing for post functionality.

--- server/server.py
+++ server/server.py
@@ -10,6 +10,37 @@ binary_extensions = {
     ".pdf"
 }
 
+def find_mime(ext) -> str:
+    match ext:
+        case ".html" | ".htm":
+            return "text/html"
+        case ".css":
+            return "text/css"
+        case ".js":
+            return "application/javascript"
+        case ".png":
+            return "image/png"
+        case ".jpg":
+            return "image/jpg"
+        case ".jpeg":
+            return "image/jpeg"
+        case ".gif":
+            return "image/gif"
+        case ".ico":
+            return "image/x-icon"
+        case ".svg":
+            return "image/svg+xml"
+        case ".webp":
+            return "image/webp"
+        case ".json":
+            return "application/json"
+        case ".pdf":
+            return "application/pdf"
+        case ".txt":
+            return "text/plain"
+        case ".xml":
+            return "application/xml"
+
 def read_text(path:str):
     try:
         with open(path, "r") as file:
@@ -47,16 +78,17 @@ def handle_client(conn: socket):
         print(f" Error with request: {request}\n")
         return
 
-    words = request_line.split(" ")
+    method, uri, _protocol = request_line.split(" ")
 
-    match words[0]:
+    match method[0]:
         case "GET":
-            uri = words[1]
             response = get(uri)
             conn.send(response)
         case _:
             response = "HTTP/1.1 501 NOT IMPLEMENTED\r\n"
             conn.send(response.encode())
+
+    conn.close()
 
 def get(uri: str) -> bytes:
     filename, extension = os.path.splitext(uri)
@@ -71,7 +103,14 @@ def get(uri: str) -> bytes:
 
     content, length, status_line = read_bin(path) if extension in binary_extensions else read_text(path)
 
-    headers = f"{status_line}\r\nContent-Length: {length}\r\n\r\n".encode()
+    mime_type = find_mime(extension)
+
+    headers = (f"{status_line}\r\n"
+               f"Content-Length: {length}\r\n"
+               f"Content-Type: {mime_type}\r\n"
+               f"Connection: close\r\n"
+               "\r\n"
+               ).encode()
     content = content if extension in binary_extensions else content.encode()
     response = headers + content
 
--- threadpool/threadpool.py
+++ threadpool/threadpool.py
@@ -5,39 +5,42 @@ class ShutDownThread(Exception):
     pass
 
 class Worker:
-    def __init__(self, id, receiver, lock):
+    def __init__(self, id, receiver):
         self.id = id
-        self.thread = threading.Thread(target=self.job, args=(receiver, lock))
+        self.thread = threading.Thread(target=self.job, args=(receiver,), name=f"worker-{id}")
         self.thread.start()
         
 
-    def job(self, receiver, lock):
-        try:
-            with lock:
+    def job(self, receiver):
+        while True:
+            try:
                 task, *args = receiver.recv()
 
-            task(*args)
-        except ShutDownThread:
-            return
+                print(f"thread {self.id} got a job, starting...")
+                task(*args)
+                print(f"thread {self.id} finished the job")
+            except ShutDownThread:
+                return
 
 
 class ThreadPool:
     def __init__(self, size):
         self.size = size
-        self.workers = []
-        worker_lock = threading.Lock()
+        self.workers: list[Worker] = []
         (self.sender, receiver) = mpsc.channel()
 
         for id in range(size):
-            self.workers.append(Worker(id, receiver, worker_lock))
+            self.workers.append(Worker(id, receiver))
 
     def execute(self, f, *args):
         self.sender.send((f, *args))
 
-    def end_thread():
+    def end_thread(self):
         raise ShutDownThread
     
     def shutdown(self):
         for worker in self.workers:
             self.sender.send(self.end_thread)
+        for worker in self.workers:
+            worker.thread.join()
 
